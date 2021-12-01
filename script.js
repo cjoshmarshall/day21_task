e814a0e60006bd3290cffab73c487b1ee8a3773a
Joshua Marshall <89794020+cjoshmarshall@users.noreply.github.com>
2021-12-01 06:37:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var time = 10;
+var timer = setInterval(()=>{
+  if(time <= 0){
+    clearInterval(timer);
+    var text = new Text(); 
+    var div = document.getElementById('countdown'); 
+    div.innerHTML="HAPPY INDEPENDENCE DAY" 
+  }else{
+    document.getElementById("countdown").innerHTML = time + " seconds remaining";
+  }
+  time -= 1;
+}, 1000);
