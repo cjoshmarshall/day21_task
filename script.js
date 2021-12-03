707d81374ef82f923a12afe7e026b4dd3275382e
Joshua Marshall <89794020+cjoshmarshall@users.noreply.github.com>
2021-12-03 20:22:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var time = 10;
 var timer = setInterval(()=>{
   if(time <= 0){
     clearInterval(timer);
-    var div = document.getElementById('countdown'); 
+    var div = document.getElementById("countdown");
     div.innerHTML="HAPPY INDEPENDENCE DAY" 
   }else{
     document.getElementById("countdown").innerHTML = time + " seconds remaining";
